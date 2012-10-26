5aaae9f08378f48c7aed5af22a52d520eccd6a37
Jeroen Frans <mrfrans@gmail.com>
2012-10-26 17:52:22 +0200

    Twitter garden
    updated code from what I had.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Twitter-Garden-SL
 =================
 
 Scripts that retrieve twitter searches and display them in Second Life through floating text.
+
+
+This uses 2 parts, a php file on a webserver and LSL code inside of SL. 
+This code is already over 3 years old, and might not be the best or most efficient way to do this.
+
+
+
---
+++ twitter-search.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+$query = $_REQUEST['query'];
+$rpp = $_REQUEST['rpp'];
+
+$c = curl_init();
+
+curl_setopt($c, CURLOPT_URL, "http://search.twitter.com/search.json?rpp=" . $rpp . "&q=" . $query);
+curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+
+curl_setopt($c, CURLOPT_USERAGENT, "Second Life object ");
+
+//curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+
+
+$response = @curl_exec($c);
+
+if (chop($response)) {
+        // We got a non-blank response, so try to parse it as JSON
+        $json = json_decode($response, TRUE);
+        curl_close($c);
+       //echo $json;
+        // Return the parsed data
+       
+        for ( $i=0; $i<$rpp; $i++ ){
+        	
+            $from_user = ereg_replace("%", "%25",$json[results][$i][from_user] );
+            $text = html_entity_decode(ereg_replace("%", "%25",$json[results][$i][text])) ;
+            print $from_user . ":^:";
+            print $text. ":^:";
+            print "http://twitter.com/" . $json[results][$i][from_user] . "/statuses/" . $json[results][$i][id] . ":*:";
+        }
+    }
+?>
